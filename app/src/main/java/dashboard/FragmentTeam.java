785008b25f0133d3f97a8d6098c5673b6ccc2e4f
Spaja <samir.spahic87@gmail.com>
2017-07-30 19:41:59 +0200

    sdada

--- app/src/main/java/dashboard/FragmentTeam.java
+++ app/src/main/java/dashboard/FragmentTeam.java
@@ -36,8 +36,6 @@ public class FragmentTeam extends Fragment {
     @Override public View onCreateView(LayoutInflater inflater,
         ViewGroup container, Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.fragment_news, container, false);
-
-        return rootView;
+        return inflater.inflate(R.layout.fragment_news, container, false);
     }
 }
