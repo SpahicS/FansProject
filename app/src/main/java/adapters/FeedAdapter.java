0a502ae9c089218a8c6c5b7b41e75314b800bba9
Haris Muharemovic <harismuharemovic@Hariss-MacBook-Pro.local>
2017-12-09 21:33:39 +0400

    asdasd

--- app/src/main/java/adapters/FeedAdapter.java
+++ app/src/main/java/adapters/FeedAdapter.java
@@ -1,6 +1,7 @@
 package adapters;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
@@ -18,6 +19,7 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -120,6 +122,7 @@ public class FeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
         });
 
         setupRecyclerViewScroll();
+
         //        this.mFeedDatabase.addValueEventListener(new ValueEventListener() {
 //
 //            @Override
@@ -192,8 +195,8 @@ public class FeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
                 break;
         }
 
-//        if (shouldAnimateListItems)
-//            AppHelper.animateItemAppearance(holder.itemView, position);
+        if (shouldAnimateListItems)
+            AppHelper.animateItemAppearance(holder.itemView, position);
 
     }
 
@@ -620,6 +623,12 @@ public class FeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
             public void onClick(View v) {
 
                 mCommentsCont.setVisibility(View.GONE);
+
+                View view = mActivity.getCurrentFocus();
+                if (view != null) {
+                    InputMethodManager imm = (InputMethodManager) mActivity.getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                }
             }
         });
 
@@ -635,7 +644,6 @@ public class FeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
             mDataSet.addAll(feedItems);
             mDataSet.add(0, new FeedItem());
             notifyDataSetChanged();
-            Toast.makeText(mActivity, "Loaded More", Toast.LENGTH_SHORT).show();
         }
 
         if (mActivity.findViewById(R.id.progressBarFeed) != null)
